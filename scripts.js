242ec3d86fb9c3dea5b553361cd7e899701e390e
MyPetLobster <6979547+MyPetLobster@users.noreply.github.com>
2023-12-02 17:47:03 -0500

    Clean up code. Ready for JS

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
-let isOn = false;
+/////// VARIABLE DECLARATIONS ///////
 
+let isOn = false;
 
 let displayText = document.querySelector(".display-text");
 let onClear = document.querySelector("#on-c");
 let displayInner = document.querySelector(".display-inner");
 let solarPanel = document.querySelector(".solar-panel");
 
+
+/////// FUNCTION DEFINITIONS ///////
 
 function powerUpCalc() {
     displayInner.style.backgroundColor = "#728173";
@@ -26,23 +29,29 @@ function brightenDisplay () {
 }
 
 
+/////// EVENT LISTENERS and ACTIONS ///////
+
+// Solar Easter Egg
 solarPanel.addEventListener("mouseover", () => {
-    dimDisplay();
+    if (isOn) {
+        dimDisplay();
+    }
 })
 solarPanel.addEventListener("mouseout", () => {
-    brightenDisplay();
+    if (isOn) {
+        brightenDisplay();
+    }
 })
-
-
-
+// Clear Button
 onClear.addEventListener("click", () => {
     if (isOn === false) {
         powerUpCalc();
         isOn === true;
-        
     }
-    displayText.innerHTML = "0.";
+    displayText.textContent = "0.";
 })
+
+
 
 
 
