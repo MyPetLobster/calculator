959786a74b3b3199618c3b1bc9bf872a63ac23c7
MyPetLobster <6979547+MyPetLobster@users.noreply.github.com>
2023-12-02 15:44:50 -0500

    Add solar panel dim & brighten funcs

--- scripts.js
+++ scripts.js
@@ -1,10 +1,38 @@
 let isOn = false;
 
 
-
 let displayText = document.querySelector(".display-text");
 let onClear = document.querySelector("#on-c");
 let displayInner = document.querySelector(".display-inner");
+let solarPanel = document.querySelector(".solar-panel");
+
+
+function powerUpCalc() {
+    displayInner.style.backgroundColor = "#728173";
+    displayInner.style.color = "black";
+}
+
+function dimDisplay() {
+    displayInner.style.transition = "all 5s ease-in-out";
+
+    displayInner.style.backgroundColor = "#28393D";
+    displayInner.style.color = "#263638";
+}
+function brightenDisplay () {
+    displayInner.style.backgroundColor = "#728173";
+    displayInner.style.color = "black";
+
+    displayInner.style.transition = "all 5s ease-in-out";
+}
+
+
+solarPanel.addEventListener("mouseover", () => {
+    dimDisplay();
+})
+solarPanel.addEventListener("mouseout", () => {
+    brightenDisplay();
+})
+
 
 
 onClear.addEventListener("click", () => {
@@ -18,7 +46,4 @@ onClear.addEventListener("click", () => {
 
 
 
-function powerUpCalc() {
-    displayInner.style.backgroundColor = "#728173";
-    displayText.style.color = "black";
-}
+
